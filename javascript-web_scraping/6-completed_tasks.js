5e9038bc199bce7afc616067dc754374aed6081f
gurbanovnihad <7404@holbertonstudents.com>
2024-05-19 17:59:29 +0400

    compute api results data

---
+++ javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,30 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const url = process.argv[2];
+
+request.get(url, (err, response, body) => {
+  if (err) {
+    console.error('Error:', err);
+    return;
+  }
+  
+  try {
+    const data = JSON.parse(body);
+    const completedTasks = {};
+
+    data.forEach(task => {
+      if (task.completed) {
+        if (!completedTasks[task.userId]) {
+          completedTasks[task.userId] = 0;
+        }
+        completedTasks[task.userId]++;
+      }
+    });
+
+    console.log(completedTasks);
+  } catch (e) {
+    console.error('Error parsing JSON:', e);
+  }
+});
