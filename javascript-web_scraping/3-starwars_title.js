325e97065047172b64d6216373b95f4c2aa481f3
gurbanovnihad <7404@holbertonstudents.com>
2024-05-19 16:36:32 +0400

    get title from api request

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -2,15 +2,15 @@
 
 const request = require('request');
 
-const root = "https://swapi-api.hbtn.io/api/films/";
+const root = 'https://swapi-api.hbtn.io/api/films/';
 const movieId = process.argv[2];
 const url = `${root}${movieId}`;
 
 request.get(url, (err, response, body) => {
   if (err) {
-    console.error("Error: ", err)
+    console.error('Error: ', err);
   } else {
     const data = JSON.parse(body);
-    console.log(data.title)
+    console.log(data.title);
   }
-})
+});
