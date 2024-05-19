9d388fef78bacbdc525b7f348b725bdb73c31e0c
gurbanovnihad <7404@holbertonstudents.com>
2024-05-19 15:41:00 +0400

    js write file

---
+++ javascript-web_scraping/1-writeme.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+
+const filePath = process.argv[2];
+const content = process.argv[3];
+
+fs.writeFile(filePath, content, 'utf-8', (err) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+
+  console.log(`Content written to ${filePath}`)
+})
