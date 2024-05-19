d080c325b176173a0a514afefda4fb6b43c957a1
gurbanovnihad <7404@holbertonstudents.com>
2024-05-19 18:15:46 +0400

    print characters promise async await

---
+++ javascript-web_scraping/101-starwars_characters.js
@@ -0,0 +1,37 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const movieId = process.argv[2];
+const url = `https://swapi-api.hbtn.io/api/films/${movieId}/`;
+
+function fetchCharacter (characterUrl) {
+  return new Promise((resolve, reject) => {
+    request(characterUrl, (error, response, body) => {
+      if (error) {
+        reject(error);
+      } else {
+        resolve(JSON.parse(body).name);
+      }
+    });
+  });
+}
+
+request(url, async (error, response, body) => {
+  if (error) {
+    console.error(error);
+    return;
+  }
+
+  const filmData = JSON.parse(body);
+  const characters = filmData.characters;
+
+  for (const characterUrl of characters) {
+    try {
+      const name = await fetchCharacter(characterUrl);
+      console.log(name);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+});
