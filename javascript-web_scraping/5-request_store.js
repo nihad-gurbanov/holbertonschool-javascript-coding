673156d4547518f4a4d0b36ce97f6fe06661adbe
gurbanovnihad <7404@holbertonstudents.com>
2024-05-19 17:19:08 +0400

    scrape web content and save to file

---
+++ javascript-web_scraping/5-request_store.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+
+const url = process.argv[2];
+const filePath = process.argv[3];
+
+const request = require('request');
+fs = require('fs');
+
+request.get(url, (err, response, body) => {
+  if (err) {
+    console.error(err);
+  } else {
+    fs.writeFile(filePath, response.body, 'utf-8', (err) => {
+      if (err) {
+        console.log(err);
+      }
+    });
+  }
+});
