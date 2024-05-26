a9d4384c873c7f53c2fc2f569932edee18808d7c
gurbanovnihad <7404@holbertonstudents.com>
2024-05-26 22:43:41 +0400

    more complex http server

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -1,25 +1,40 @@
 const http = require('http');
-const countStudents = require('./3-read_file_async');
+const fs = require('fs');
 
 const app = http.createServer((req, res) => {
   if (req.url === '/') {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello Holberton School!');
+    res.end('Hello Holberton School!\n');
   } else if (req.url === '/students') {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    countStudents(process.argv[2])
-      .then((data) => {
-        res.end(data);
-      })
-      .catch((error) => {
-        res.end(error.message);
-      });
+    const database = process.argv[2];
+    if (!database) {
+      res.writeHead(500, { 'Content-Type': 'text/plain' });
+      res.end('Error: No database provided\n');
+      return;
+    }
+    fs.readFile(database, 'utf8', (err, data) => {
+      if (err) {
+        res.writeHead(500, { 'Content-Type': 'text/plain' });
+        res.end(`Error reading file: ${err.message}\n`);
+        return;
+      }
+      const students = data.trim().split('\n').filter((line) => line.trim() !== '');
+      const studentsInCS = students.filter((student) => student.endsWith('CS')).map((student) => student.split(',')[0]);
+      const studentsInSWE = students.filter((student) => student.endsWith('SWE')).map((student) => student.split(',')[0]);
+      res.writeHead(200, { 'Content-Type': 'text/plain' });
+      res.write('This is the list of our students\n');
+      res.write(`Number of students: ${students.length}\n`);
+      res.write(`Number of students in CS: ${studentsInCS.length}. List: ${studentsInCS.join(', ')}\n`);
+      res.end(`Number of students in SWE: ${studentsInSWE.length}. List: ${studentsInSWE.join(', ')}\n`);
+    });
   } else {
     res.writeHead(404, { 'Content-Type': 'text/plain' });
-    res.end('Not found');
+    res.end('404 Not Found\n');
   }
 });
 
-app.listen(1245);
+app.listen(1245, () => {
+  console.log('Server is running on port 1245');
+});
 
 module.exports = app;
